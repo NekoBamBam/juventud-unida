18ba81a2e7ffcb34497a7257e291ea24ce1a41c4
NekoBamBam <coriannicolas21@gmail.com>
2025-08-02 23:51:22 -0300

    mapa e info

--- src/components/Carrousel.jsx
+++ src/components/Carrousel.jsx
@@ -36,13 +36,37 @@ function Carrusel() {
         onClick={anterior}
         className="absolute left-0 top-1/2 transform -translate-y-1/2 bg-white/80 p-2 rounded-full shadow hover:bg-white"
       >
-        ◀
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="text-black bi bi-arrow-left"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"
+          />
+        </svg>
       </button>
       <button
         onClick={siguiente}
         className="absolute right-0 top-1/2 transform -translate-y-1/2 bg-white/80 p-2 rounded-full shadow hover:bg-white"
       >
-        ▶
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="16"
+          height="16"
+          fill="currentColor"
+          className="text-black bi bi-arrow-right"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fill-rule="evenodd"
+            d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8"
+          />
+        </svg>
       </button>
     </div>
   );
--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
 
 function Info() {
   return (
-    <div>Info</div>
-  )
+    <div className="p-6">
+      <h2 className="text-3xl font-bold mb-6 text-center">¿Dónde estamos?</h2>
+
+      <div className="flex flex-col md:flex-row items-center md:items-start gap-6">
+        {/* Mapa */}
+        <div className="w-full md:w-1/2 h-[300px] md:h-[400px]">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d9259.424780439895!2d-57.900934816683765!3d-34.863738905043526!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e577da97df67%3A0x8e10fdbbfc8e2142!2sJuventud%20Unida%20Ensenada!5e0!3m2!1ses-419!2sar!4v1754189026695!5m2!1ses-419!2sar"
+            width="600"
+            height="450"
+            style={{ border: 0 }}
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+        </div>
+
+        {/* Texto descriptivo */}
+        <div className="w-full md:w-1/2 text-white">
+          <p className="text-lg mb-4">
+            Nombre Completo: Club Social, Cultural y Deportivo Juventud Unida de Ensenada
+          </p>
+          <p>
+            Fecha de Fundación: 7 de Enero de 2015
+          </p>
+          <p>
+            Sede: Ortiz de Rosas y Almafuerte
+Predio Infantil: Ingeniero Medici y Almafuerte
+          </p>
+          <p>
+            Club fundador de la Liga Amistad, liga en la que compite con 8 titulos obtenidos hasta la fecha
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Info
+export default Info;
